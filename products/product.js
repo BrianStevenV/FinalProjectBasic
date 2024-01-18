b9456af461ea81ac274846f06e4bbce0266b2957
Brian Steven <sbrianprog@gmail.com>
2024-01-18 13:29:03 -0500

    responsive design breakpoint 480px and 768px

---
+++ products/product.js
@@ -0,0 +1,18 @@
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('#navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('header__ul');
+}
+
+
+
+let menuMainSection = document.querySelector('#menu-icon-section');
+let navlistMainSection = document.querySelector('#navlist-section');
+
+menuMainSection.onclick = () => {
+    menuMainSection.classList.toggle('bx-x');
+    navlistMainSection.classList.toggle('section__nav--ul');
+}
