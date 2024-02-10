b65be34123687728cbeb0cc7320bf0361700b611
Brian Steven <sbrianprog@gmail.com>
2024-02-09 23:04:02 -0500

    add payment.js

--- buying/payment.js
+++ buying/payment.js
@@ -1,6 +1,7 @@
 const userData = sessionStorage.getItem('formData');
 const userDataParse = JSON.parse(userData);
-console.log(userDataParse);
+
+const productData = sessionStorage.getItem('productList');
 
 const {cardName, cardMethod} = userDataParse;
 
@@ -28,10 +29,61 @@ const orderDate = () => {
 const dateElement = document.getElementById("purchase__date");
 const orderNumberElement = document.getElementById("purchase__orderNumber");
 const nameElement = document.getElementById("purchase__name");
+const methodElement = document.getElementById("purchase__method");
+const productContainer = document.querySelectorAll(".product__card");
+
 
 dateElement.textContent = orderDate();
 orderNumberElement.textContent = orderNumberRandom();
 nameElement.textContent = cardName;
+methodElement.textContent = cardMethod
 
+const printProducts = () => {
+    console.log(`object`);
+    productContainer.forEach(product => {
+        product.innerHTML =""
+    })
+    const productListString = sessionStorage.getItem('productList');
+    const productList = JSON.parse(productListString);
+    let htmlContent = ''; 
+
+    productList.forEach( product => {
+        const createArticle = document.createElement('article');
+        createArticle.classList.add('product__card');
+
+        const addedProduct = `
+                        <figure class="product__card--figure">
+                            <img class="product__card--img" src="${product.images[0]}" alt="${product.name}">
+                        </figure>
+
+                        <article class="product__card--info">
+                            <h1 class="product__card--info--h1">
+                                ${product.name}
+                            </h1>
+                            <p class="product__card--info--p--code">
+                                ${product.code}
+                            </p>
+                            <p class="product__card--info--p--quantity">
+                                x1
+                            </p>
+                        </article>
+
+                        <article class="product__card--price">
+                            <p class="product__card--price--p">
+                                $${product.price}
+                            </p>
+                        </article>
+        `
+        createArticle.innerHTML = addedProduct;
+        htmlContent += createArticle;
+        document.body.appendChild(createArticle);
+    })
+
+    return htmlContent;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    printProducts();
+})
 
 
