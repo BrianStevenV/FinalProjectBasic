88ed8134cf9575b675f30e317fa8e50e10404255
Brian Steven <sbrianprog@gmail.com>
2024-02-09 19:54:24 -0500

    cart

--- cart/cart.js
+++ cart/cart.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   const detailsContainer = document.querySelector(".card__product__container");
 
@@ -6,184 +7,121 @@ document.addEventListener('DOMContentLoaded', function() {
 
       if (purchaseButton) {
           console.log('Purchase button clicked');
-          // Agrega aquí el código que deseas ejecutar cuando se hace clic en el botón "Buy now"
+          
           createCartSection();
       }
   });
 });
 
+
+
+const createFigureContainer = () => {
+  const figureContainer = document.createElement('figure');
+  figureContainer.className.add('card');
   
+  var addedProduct = `
+          <figure
+            <img class="cart__img" src="${product.images}" alt="img" />
+            <figcaption class="cart__figcaption">
+              <section class="cart__info">
+                  <p class="title__container">${product.name}</p>
 
-  
+                  <p  class="code__container">Code:${product.code}</p>
 
-  function createCartSection() {
-    console.log(`Hola`);
-    // innerHTML='';
-    const bodyCartSection = document.createElement('section');
-    bodyCartSection.className = 'body__cart';
-  
-    var cartContent = `
-      <section class="body__cart">
-        <section class="your__cart">
-          <article class="your__cart--h1__p__container">
-            <h1 class="h1__p__container--h1">Your cart</h1>
-            <div class="bx bx-x" id="close-icon"></div>
-          </article>
-
-          <section class="cart__container">
-            <figure class="cart">
-              <img class="cart__img" src="../assets/products_img/Img 2-b page 2.png" alt="img" />
-              <figcaption class="cart__figcaption">
-                <section class="cart__info">
-                  <p class="title__container">Luxury Charms Ring</p>
-
-                  <p  class="code__container">Code:239847</p>
-
-                  <p class="price__container">$678.71</p>
-                </section>
-
-                <section class="cart__misc">
-                  <article class="edit__container">
-                    <p>✏</p>
-                  </article>
-
-                  <article class="quantity__container">
-                    <p>x1</p>
-                  </article>
-                </section>
-              </figcaption>
-            </figure>
-
-            <figure class="cart">
-              <img class="cart__img" src="../assets/products_img/Img 2-b page 2.png" alt="img" />
-              <figcaption class="cart__figcaption">
-                <section class="cart__info">
-                  <p class="title__container">Luxury Charms Ring</p>
-
-                  <p  class="code__container">Code:239847</p>
-
-                  <p class="price__container">$678.71</p>
-                </section>
-
-                <section class="cart__misc">
-                  <article class="edit__container">
-                    <p>✏</p>
-                  </article>
-
-                  <article class="quantity__container">
-                    <p>x1</p>
-                  </article>
-                </section>
-              </figcaption>
-            </figure>
-
-            <figure class="cart">
-              <img class="cart__img" src="../assets/products_img/Img 2-b page 2.png" alt="img" />
-              <figcaption class="cart__figcaption">
-                <section class="cart__info">
-                  <p class="title__container">Luxury Charms Ring</p>
-
-                  <p  class="code__container">Code:239847</p>
-
-                  <p class="price__container">$678.71</p>
-                </section>
-
-                <section class="cart__misc">
-                  <article class="edit__container">
-                    <p>✏</p>
-                  </article>
-
-                  <article class="quantity__container">
-                    <p>x1</p>
-                  </article>
-                </section>
-              </figcaption>
-            </figure>
-
-            <figure class="cart">
-              <img class="cart__img" src="../assets/products_img/Img 2-b page 2.png" alt="img" />
-              <figcaption class="cart__figcaption">
-                <section class="cart__info">
-                  <p class="title__container">Luxury Charms Ring</p>
-
-                  <p  class="code__container">Code:239847</p>
-
-                  <p class="price__container">$678.71</p>
-                </section>
-
-                <section class="cart__misc">
-                  <article class="edit__container">
-                    <p>✏</p>
-                  </article>
-
-                  <article class="quantity__container">
-                    <p>x1</p>
-                  </article>
-                </section>
-              </figcaption>
-            </figure>
-        
-          <hr class="footer__hr" />
-          <section class="cart__footer">
-            <article class="cart__footer--total__price">
-              <p class="total__price--total">Total:</p>
-              <p class="total__price--price">$621.75</p>
-            </article>
-            <article class="cart__footer--button__container">
-              <button class="cart__footer--button__container--button">
-                Continue to check out
-              </button>
-            </article>
-          </section>
-        </section>
-      </section>
-    `;
-  
-    bodyCartSection.innerHTML = cartContent;
-  
-    const print = document.body.appendChild(bodyCartSection);
-    console.log(`prtin print print ${print}`);
-
-  }
-
-//averiguar como hacer que se aplique el estilo brightness/contrast al fondo al abrir el carrito
-   const printProducts = (listProducts, container) =>{
-
-    console.log(`From print product`);
-    container.innerHTML = '';
-    listProducts.foreach(Product => {
-
-      var cartSection = document.createElement('figure')
-      cartSection.classList.add('card')
-  
-      var addedProduct = `
-            <img src="${Product.image}" alt="img" />
-            <figcaption class="card__figcaption">
-              <section class="card__info">
-                  <p class="title__container">${Product.name}</p>
-
-                  <p  class="code__container">Code:${Product.code}</p>
-
-                  <p class="price__container">$${Product.price}</p>
+                  <p class="price__container">$${product.price}</p>
               </section>
-              <section class="card__misc">
-                <article class="edit__ccontaine">
+              <section class="cart__misc">
+                <article class="edit__container">
                   <p>✏</p>
                 </article>
 
-                <article class="cuantity__container">
+                <article class="quantity__container">
                   <p>x1</p>
                 </article>
               </section>
             </figcaption>`;
+}
+  
 
-        cartSection.innerHTML = addedProduct;
-        container.appendChild(figure)
+function createCartSection() {
 
-    })
+  const productListString = sessionStorage.getItem('productList');
+  const productList = JSON.parse(productListString);
+  
+  const bodyCartSection = document.createElement('section');
+  bodyCartSection.className = 'body__cart';
 
-    
+  
+  const cartContainer = document.createElement('section');
+  cartContainer.className = 'cart__container';
 
+  var cartContent = `
+    <section class="your__cart">
+      <article class="your__cart--h1__p__container">
+        <h1 class="h1__p__container--h1">Your cart</h1>
+        <div class="bx bx-x" id="close-icon"></div>
+      </article>
+
+      <section class="cart__container">
+          ${printProducts(productList, cartContainer)}
+      </section>
+      
+      <hr class="footer__hr" />
+      
+      <section class="cart__footer">
+        <article class="cart__footer--total__price">
+          <p class="total__price--total">Total:</p>
+          <p class="total__price--price">$621.75</p>
+        </article>
+        <article class="cart__footer--button__container">
+          <button class="cart__footer--button__container--button">
+            Continue to check out
+          </button>
+        </article>
+      </section>
+    </section>
+  `;
+
+  bodyCartSection.innerHTML = cartContent;
+
+  const print = document.body.appendChild(bodyCartSection);
+  console.log(`print print print ${print.textContent}`);
+  console.log(`Var content ${bodyCartSection.textContent}`);
+}
+
+
+const printProducts = (listProduct) => {
+  let htmlContent = '';  
+
+  listProduct.forEach(product => {
+      console.log(`From for each print products`);
+
+      var cartSection = document.createElement('figure')
+      cartSection.classList.add('cart')
+
+      var addedProduct = `
+          <img class="cart__img" src="${product.images[0]}" alt="img" />
+          <figcaption class="cart__figcaption">
+              <section class="cart__info">
+                  <p class="title__container">${product.name}</p>
+                  <p class="code__container">Code:${product.code}</p>
+                  <p class="price__container">$${product.price}</p>
+              </section>
+              <section class="cart__misc">
+                  <article class="edit__container">
+                      <p>✏</p>
+                  </article>
+                  <article class="quantity__container">
+                      <p>x1</p>
+                  </article>
+              </section>
+          </figcaption>`;
+
+      cartSection.innerHTML = addedProduct;
+      htmlContent += cartSection.outerHTML;  
+  });
+
+  return htmlContent;  
 }
 
 
